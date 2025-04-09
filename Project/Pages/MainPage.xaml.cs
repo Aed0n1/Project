f39f79ca7da1d55e639ffd59154fd2561b89fdc3
Aed0n1 <alexnechiporuk8@gmail.com>
2025-04-09 16:07:20 +0200

    First commit

---
+++ Project/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace Project.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
+
