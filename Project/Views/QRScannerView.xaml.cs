3a24d8c2e45590fa4ac25d49fc3a96412f750c51
Aed0n1 <alexnechiporuk8@gmail.com>
2025-04-15 22:39:16 +0200

    Fourth commit

--- Project/Views/QRScannerView.xaml.cs
+++ Project/Views/QRScannerView.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -15,14 +17,52 @@ using System.Windows.Shapes;
 
 namespace Project.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для QRScannerView.xaml
-    /// </summary>
     public partial class QRScannerView : UserControl
     {
         public QRScannerView()
         {
             InitializeComponent();
         }
+
+        public void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Переключаемся на MainWindow
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow.BackToMainWindow();
+        }
+        private void ProductNumberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Разрешаем только цифры
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        // Показываем Popup при наведении мышки на TextBox
+        private void ProductNumberTextBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var popup = (Popup)ProductNumberTextBox.Template.FindName("PopupToolTip", ProductNumberTextBox);
+            if (popup != null)
+            {
+                popup.IsOpen = true; // Показываем popup
+            }
+        }
+
+        // Скрываем Popup при убирании мышки с TextBox
+        private void ProductNumberTextBox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var popup = (Popup)ProductNumberTextBox.Template.FindName("PopupToolTip", ProductNumberTextBox);
+            if (popup != null && !IsMouseOverPopup(popup)) // Проверяем, что курсор не над Popup
+            {
+                popup.IsOpen = false; // Скрываем popup
+            }
+        }
+
+        // Проверяем, что курсор не находится над Popup
+        private bool IsMouseOverPopup(Popup popup)
+        {
+            var popupContent = (UIElement)popup.Child;
+            return popupContent.IsMouseOver || popup.IsMouseOver;
+        }
+
+
     }
 }
