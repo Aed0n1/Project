b26c251fc8b16483a1d4f8a2bb9ca2894dd4b1fe
Aed0n1 <alexnechiporuk8@gmail.com>
2025-04-21 15:11:52 +0200

    Seventh commit

--- Project/Views/SettingsView.xaml.cs
+++ Project/Views/SettingsView.xaml.cs
@@ -27,5 +27,38 @@ namespace Project.Views
             var mainWindow = (MainWindow)Application.Current.MainWindow;
             mainWindow.BackToMainWindow();
         }
+        private void LanguageComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LanguageComboBox.SelectedItem is ComboBoxItem selectedItem && selectedItem.Tag is string langCode)
+            {
+                ChangeLanguage(langCode);
+            }
+        }
+
+        private void ChangeLanguage(string languageCode)
+        {
+            var dict = new ResourceDictionary();
+            switch (languageCode)
+            {
+                case "pl":
+                    dict.Source = new System.Uri("/Resources/StringResources.pl.xaml", System.UriKind.Relative);
+                    break;
+                case "en":
+                default:
+                    dict.Source = new System.Uri("/Resources/StringResources.en.xaml", System.UriKind.Relative);
+                    break;
+            }
+
+            // Удаляем старые словари и добавляем новый
+            var oldDict = Application.Current.Resources.MergedDictionaries
+                .FirstOrDefault(d => d.Source != null && d.Source.OriginalString.Contains("StringResources"));
+
+            if (oldDict != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+            }
+
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
     }
 }
